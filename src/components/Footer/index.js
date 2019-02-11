2f9ac815d9146104b449c0b91fe9dc6eb28bfa32
Rebecca Baker <rebecca122@mac.com>
2019-02-11 10:41:13 -0600

    final clicky

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Thanks for playing "The Office" Clicky Game!
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Welcome to</h1><p></p>
+            <h1>"The Office"</h1><p></p><h1>Clicky Game!</h1><p></p>
+            <h2>See How Well You Remember Your Choices.  Select each picture ONLY ONCE.  Click twice and you lose the game. <p></p> Click all 12 without repeating, and you WIN!!</h2>
+        </header>
+    );
+}
+
+export default Header;
